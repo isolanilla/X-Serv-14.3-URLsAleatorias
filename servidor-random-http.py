65967f820261bb76e948cdbe9aff95284fa30dce
isolanilla <i.solanilla3@gmail.com>
2015-02-15 21:45:37 +0100

    Ejercicio_Sin_pasar_pep8

---
+++ servidor-random-http.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+"""
+Ejercicio URL aleatorias
+Ismael Solanilla
+"""
+
+import socket
+import random
+
+# Create a TCP objet socket and bind it to a port
+# Port should be 80, but since it needs root privileges,
+# let's use one above 1024
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind((socket.gethostname(), 1236))
+mySocket.listen(5)
+
+# Accept connections, read incoming data, and answer back an HTML page
+#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
+
+try:
+    while True:
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        print 'Request received:'
+        print recvSocket.recv(2048)
+        print 'Answering back...'
+        recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                        "<html><body><h1>HOLA ANGELA!!!</h1>" +
+                        "<a href='"+str(random.randrange(123456789))+"'>dame otra</a>"+
+                        "</body></html>" +
+                        "\r\n")
+        recvSocket.close()
+except KeyboardInterrupt:
+    print "Closing binded socket"
+    mySocket.close()
